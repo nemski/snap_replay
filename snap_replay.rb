32a1b34ca0a05b1b70017fd86f80e40777729210
Pat <nemski.rabbit@gmail.com>
2013-06-03 00:03:19 +1000

    process get[-next] requests
    Use pduType to determine snmp request type

--- snap_replay.rb
+++ snap_replay.rb
@@ -6,36 +6,31 @@ require_relative 'snap_parser'
 # SNMP datatypes
 #
 ASN1DataTypes = {
-       :"INTEGER-32"           =>      2,
-       :"OCTET-STRING"     =>  4,
-       :NULL                           =>      5,
-       :"OBJ-ID"                       =>      6,
-       :SEQUENCE           =>  48,
-       :"IP-ADDRESS"           =>      64,
-       :"COUNTER-32"           =>      65,
-       :"GAUGE-32"                     =>      66,
-       :"TIME-TICKS"           =>      67,
-       :"COUNTER-64"           =>      70,
+   :"INTEGER-32"           			=>      2,
+   :"OCTET-STRING"     				=> 		4,
+   :NULL                           	=>      5,
+   :"OBJ-ID"                       	=>      6,
+   :SEQUENCE           				=> 		48,
+   :"IP-ADDRESS"           			=>      64,
+   :"COUNTER-32"           			=>      65,
+   :"GAUGE-32"                     	=>      66,
+   :"TIME-TICKS"           			=>      67,
+   :"COUNTER-64"           			=>      70,
 
 }
 
-if ! ARGV[0]
-	puts "Usage: #{$0} <snmp snap>\n"
-	exit
-end
-
 def parse_reply(pkt)
-  return if not pkt[1]
+	return if not pkt[1]
 
-  asn1 = OpenSSL::ASN1.decode(pkt) rescue nil
-  if(! asn1)
-               puts "Not ASN encoded data"
-               return
-  end
+	asn1 = OpenSSL::ASN1.decode(pkt) rescue nil
+	if(! asn1)
+	           puts "Not ASN encoded data"
+	           return
+	end
 
-  msgVersion = asn1.value[0].value
+	msgVersion = asn1.value[0].value
 
-  if msgVersion == 3
+	if msgVersion == 3
 	  msgGlobalData = asn1.value[1]
 
 	  msgData = asn1.value[3]
@@ -43,44 +38,49 @@ def parse_reply(pkt)
 	  contextName = msgData.value[1]
 
 	  pdu = msgData.value[2]
-	  requestId = pdu.value[0]
-	  errorStatus = pdu.value[1]
-	  errorIndex = pdu.value[2]
-	  varBind = pdu.value[3]
 
-	  if varBind
-	       var = varBind.value[0]
-	       if var
-        	       oid = var.value[0]
-	               val = var.value[1]
-	       end
-	  end
+	elsif msgVersion == 1
+		community = asn1.value[1]
+		pdu = asn1.value[2]
+		pduType = pdu.tag
 
-	  snmpResult = {  "msgAuthEngineID"               => msgAuthoritiveEngineID,
-                               "msgAuthEngineBoots"    => msgAuthoritiveEngineBoots,
-                               "msgAuthEngineTime"             => msgAuthoritiveEngineTime,
-                               "contextEngineID"          => contextEngineID,
-                               "errorStatus"              => errorStatus,
-                               "oid"                              => oid,
-                               "val"                              => val,
-               }
-  elsif msgVersion == 1
-	community = asn1.value[1]
-	pdu = asn1.value[2]
-	
-	snmpResult = {  "pdu"		=> pdu,
-			"community"	=> community,
-		}
-  else
+	else
 	puts "Only msgVersion 1 and 3 supported"
 	puts msgVersion.methods
 	return
-  end
+	end
+
+	requestId = pdu.value[0]
+	errorStatus = pdu.value[1]
+	errorIndex = pdu.value[2]
+	varBind = pdu.value[3]
+
+	if varBind
+	    var = varBind.value[0]
+	    if var
+	       oid = var.value[0]
+           val = var.value[1]
+    	end
+	end
+
+	snmpResult = {  
+		:msgAuthEngineID        => msgAuthoritiveEngineID,
+        :msgAuthEngineBoots    	=> msgAuthoritiveEngineBoots,
+        :msgAuthEngineTime      => msgAuthoritiveEngineTime,
+		:contextEngineID        => contextEngineID,
+		:pduType				=> pduType,
+		:community				=> community,
+		:requestId				=> requestId,
+		:errorStatus            => errorStatus,
+		:errorIndex				=> errorIndex,
+		:oid                    => oid,
+		:val                    => val,
+    }
 	
-  snmpResult
+  snmpReturn
 end
 
-file = File.new(ARGV[0])
+file = "~/test.snap"
 agent = SNMPAgent.new
 
 while (line = file.gets)
@@ -88,7 +88,6 @@ while (line = file.gets)
 	case tag
 	when nil, [], "", 0
 		puts "invalid line\n"
-		next
 	else
 		# A cheap way to convert our strings into symbols, which then act as a lookup table for ASN tag hex values
 		tag = ASN1DataTypes[:"#{tag}"]
@@ -103,5 +102,14 @@ ret, sender = udp_socket.recvfrom(65535)
 
 snmpReturn = parse_reply(ret)
 
-puts snmpReturn
+case snmpReturn[:pduType]
+when 0
+	reply = agent.get(snmpReturn[:oid])
+when 1
+	reply = agent.get_next(snmpReturn[:oid])
+else
+	puts "Invalid request received"
+	exit
+end
 
+puts reply
